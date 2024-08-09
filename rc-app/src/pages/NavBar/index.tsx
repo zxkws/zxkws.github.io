0c0c33ea2eeb6a508021f44bff69eecfc1d08d2e
zxkws <loveyhcui@163.com>
2024-08-09 04:35:07 +0000

    fix

--- rc-app/src/pages/NavBar/index.tsx
+++ rc-app/src/pages/NavBar/index.tsx
@@ -60,6 +60,17 @@ const navItems = [
     { name: 'http代理', url: 'http://testt.leov.asia/linuxdo/ip.php'},
     { name: '高德交通指数', url: 'https://report.amap.com/m/dist/#/ct'},
     { name: '埋堆堆TVB', url: '#'},
+    { name: 'my sub', url: 'https://proxy.look.cloudns.biz/proxy/https://gist.githubusercontent.com/zxkws/6bb690997dc4af34b5938ecc91249f91/raw/clash.yaml'},
+    { name: '', url: ''},
+    { name: '', url: ''},
+    { name: '', url: ''},
+    { name: '', url: ''},
+    { name: '', url: ''},
+    { name: '', url: ''},
+    { name: '', url: ''},
+    { name: '', url: ''},
+    { name: '', url: ''},
+    { name: '', url: ''},
     { name: '', url: ''},
   ];
 
