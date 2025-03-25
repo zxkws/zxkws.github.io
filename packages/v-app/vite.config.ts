1b363fab1ef4b06281840fc5ab8f2ca23d148ace
zxkws <loveyhcui@163.com>
2025-03-25 06:12:34 +0000

    feat: v-app

--- packages/v-app/vite.config.ts
+++ packages/v-app/vite.config.ts
@@ -1,12 +1,9 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import htmlPlugin from "vite-plugin-index-html";
 import AutoImport from "unplugin-auto-import/vite";
 import { resolve } from "path";
-const isProd = process.env.NODE_ENV === "production";
 
-const env = loadEnv(process.env.NODE_ENV as string, process.cwd(), "");
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,16 +13,11 @@ export default defineConfig({
       // 可以在这里添加其他 JSX 相关配置
       // 比如: transformOn: true, optimize: true 等
     }),
-    htmlPlugin({
-      input: isProd ? "./src/main.ts" : "/src/main.ts",
-      preserveEntrySignatures: "exports-only",
-    }),
     AutoImport({
       imports: ["vue"],
       dts: "src/auto-imports.d.ts", // 自动生成的 TypeScript 声明文件路径
     }),
   ],
-  base: env.VITE_BASE_URL,
   css: {
     preprocessorOptions: {
       less: {
