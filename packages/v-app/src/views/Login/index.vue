aa138537bd5572a2fa523df2c86c3e2ae5ad708d
zxkws <loveyhcui@163.com>
2025-05-28 11:43:45 +0800

    Update index.vue

--- packages/v-app/src/views/Login/index.vue
+++ packages/v-app/src/views/Login/index.vue
@@ -148,7 +148,7 @@ const onSubmit = () => {
             </a>
 
             <a
-              href="https://zxkws.nyc.mn/auth/github/callback"
+              href="https://api.zxkws.nyc.mn/api/auth/github"
               class="flex w-full items-center justify-center gap-3 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus-visible:ring-transparent"
             >
               <svg
