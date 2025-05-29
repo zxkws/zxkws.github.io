dc75f7716c9d5a70d61fe54dbd3c9ecad4c1994d
zxkws <loveyhcui@163.com>
2025-05-29 10:36:53 +0800

    Update main.ts

--- packages/v-app/src/main.ts
+++ packages/v-app/src/main.ts
@@ -15,6 +15,13 @@ import router from "./router";
 let vue: Root<Element> | null = null;
 
 const runApp = (container: Element | string) => {
+const params = new URLSearchParams(window.location.search);
+const token = params.get('token');
+if(token) {
+  localStorage.setItem('auth_token',token);
+  const newUrl = window.location.origin + window.location.pathname + window.location.hash;
+  window.history.replaceState({}, '', newUrl);
+}
   vue = createApp(App);
   setLicenseKey(
     "0b50c5c2999298c91d183c696087eb90T1JERVI6MDAwMDEsRVhQSVJZPTQxMDIzNTg0MDAwMDAsRE9NQUlOPV8sS0VZVkVSU0lPTj0xLFVMVElNQVRFPTE="
