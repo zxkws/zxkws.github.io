02eabfb42737fa54e467284b68ed3003bf1b7061
zxkws <loveyhcui@163.com>
2025-09-28 09:34:30 +0000

    feat: style

--- packages/v-app/src/components/Menu.vue
+++ packages/v-app/src/components/Menu.vue
@@ -79,8 +79,11 @@ function closeMenu() {
         <ul class="space-y-2 mt-2 p-4">
             <li v-for="menu in menus" class="p-2 bg-white dark:bg-gray-700 border border-gray-300 dark:border-gray-600 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600"
             :key="menu.name">
-            <button @click="$router.push({ name: menu.name })" :title="menu.label" class="w-full flex items-center dark:text-white" :class="{'justify-center': isMenuCollapsed}">
-                <span :class="['transition-opacity', { 'opacity-0': isMenuCollapsed, 'opacity-100': !isMenuCollapsed }]" >{{ menu.label }}</span>
+            <button @click="$router.push({ name: menu.name })" :title="menu.label" class="w-full flex items-center h-8 dark:text-white" :class="{'justify-center': isMenuCollapsed}">
+              <transition name="fade" mode="out-in">
+                <span v-if="!isMenuCollapsed" class="whitespace-nowrap">{{ menu.label }}</span>
+                <span v-else>{{ menu.label.charAt(0) }}</span>
+              </transition>
             </button>
             </li>
         </ul>
@@ -88,4 +91,13 @@ function closeMenu() {
   </div>
 </template>
 <style>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
