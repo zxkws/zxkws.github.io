3bec9679e2422085341e83b83639706f25c27d66
zxkws <loveyhcui@163.com>
2025-10-20 14:33:42 +0800

    feat: update menu

--- packages/main-app/src/layouts/BasicLayout/menuConfig.ts
+++ packages/main-app/src/layouts/BasicLayout/menuConfig.ts
@@ -4,7 +4,16 @@ const headerMenuConfig: MenuItem[] = [];
 
 const asideMenuConfig: MenuItem[] = [
   { name: '首页', path: '/' },
-  { name: 'Vue 应用', path: '/v-app' },
+  {
+    name: 'Vue 应用',
+    path: '/v-app',
+    children: [
+      { name: '导航列表', path: '/v-app/navList' },
+      { name: 'Todo', path: '/v-app/todo' },
+      { name: '账户管理', path: '/v-app/account' },
+      { name: 'LLM 排名', path: '/v-app/llm-ranking' },
+    ],
+  },
   { name: '文本对比', path: '/textdiff' },
   { name: 'Curl Converter', path: '/curlconverter' },
   { name: '配置中心', path: '/config-hub' },
