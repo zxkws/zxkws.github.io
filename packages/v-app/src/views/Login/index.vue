d1d53cd35c552110981f7a14610185b6ef33313f
zxkws <loveyhcui@163.com>
2025-12-04 19:24:33 +0800

    feat: update code

--- packages/v-app/src/views/Login/index.vue
+++ packages/v-app/src/views/Login/index.vue
@@ -30,7 +30,6 @@ const onSubmit = () => {
 <template>
   <div class="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
     <div class="sm:mx-auto sm:w-full sm:max-w-md">
-      <img class="mx-auto h-10 w-auto" src="https://zxkws.nyc.mn/favicon.ico" alt="Your Company" />
       <h2 class="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
         Sign in to your account
       </h2>
