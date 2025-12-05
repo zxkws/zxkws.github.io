84b4eaa3c3f2b04246d7835c43194cfec30cbc97
zxkws <loveyhcui@163.com>
2025-12-05 09:00:51 +0800

    feat: update code

--- packages/auth-app/vite.config.ts
+++ packages/auth-app/vite.config.ts
@@ -2,15 +2,18 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
+export default defineConfig(({ mode }) => {
+  const isProd = mode === 'production';
+  return {
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, 'src'),
+      },
     },
-  },
-  server: {
-    port: 5183,
-  },
-  base: '/',
+    server: {
+      port: 5183,
+    },
+    base: isProd ? '/auth-app/' : '/',
+  };
 });
