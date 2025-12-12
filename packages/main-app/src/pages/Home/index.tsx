fb569fe0955933b7deb909c565b12a10e025c4a2
zxkws <loveyhcui@163.com>
2025-12-12 17:15:49 +0800

    feat: update code

--- packages/main-app/src/pages/Home/index.tsx
+++ packages/main-app/src/pages/Home/index.tsx
@@ -1,5 +1,9 @@
 const Home = () => {
-  return <h1>home page</h1>;
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">home page</h1>
+    </div>
+  );
 };
 
 export default Home;
